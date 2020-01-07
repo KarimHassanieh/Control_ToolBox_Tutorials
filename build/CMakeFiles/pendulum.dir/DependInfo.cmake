5c9b345f8a84a8cc57213ca54649dc4ad3fc5c42
KarimHassanieh <karimhassanieh@gmail.com>
2020-01-07 15:21:55 -0500

    Add pendulum example

---
+++ build/CMakeFiles/pendulum.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/karim/Desktop/KARIM/ROBOTICS/Control_ToolBox_Examples/simple_pendulum.cpp" "/home/karim/Desktop/KARIM/ROBOTICS/Control_ToolBox_Examples/build/CMakeFiles/pendulum.dir/simple_pendulum.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CPPAD"
+  "CPPADCG"
+  "LLVM_VERSION_MAJOR=6"
+  "LLVM_VERSION_MINOR=0"
+  "PLOTTING_ENABLED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/python2.7"
+  "/usr/include/eigen3"
+  "/usr/lib/llvm-6.0/include"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ simple_pendulum.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <ct/core/plot/plot.h>
+#include <ct/optcon/optcon.h>
+using namespace std;
+using namespace ct::core ;
+using namespace ct::core::plot;
+int main ()
+{
+
+    // a damped oscillator has two states, position and velocity
+    const size_t state_dim = SecondOrderSystem::STATE_DIM;  // = 2
+    // create a state
+    StateVector<state_dim> x;
+    StateVectorArray<state_dim> states; //To record states along trajectory
+    ct::core::tpl::TimeArray<double> time_stamps; //To record Time Stamps along trajectory
+    // we initialize it at a point with unit deflection and zero velocity
+    x(0) = 2.0;//Position
+    x(1) = 0.0;//Velocity
+    // create an oscillator, which is a predefined system in ct_core
+   // If k is spring stiffnes,m is mass and d is damper constant then we have the below equations :
+   //w_n=square root of (k/m)
+   //zeta=(d/(2*sqrt(m*k)))
+    double k=25;
+    double m=1;
+    double d=3;
+    double w_n = sqrt(k/m);
+    double zeta=(d/(2*sqrt(m*k)));
+    shared_ptr<SecondOrderSystem> oscillator(new SecondOrderSystem(w_n,zeta));
+    // create an integrator
+    Integrator<state_dim> integrator(oscillator);
+    // simulate 1000 steps
+    double dt = 0.001;
+    Time t0 = 0.0;
+    size_t nSteps = 3000;
+    states.push_back(x);
+for (size_t i = 0; i < nSteps; i++){
+    integrator.integrate_n_steps(x, i*dt, 1, dt);
+    // print the new state
+    states.push_back(x);
+    time_stamps.push_back(i*dt);
+
+}
+ // Plot Output:
+ std::vector<double> time_plot, position_plot, velocity_plot;
+
+ for (size_t i = 0; i < time_stamps.size(); i++)
+    {
+        time_plot.push_back(time_stamps[i]);
+        position_plot.push_back(states[i](0));
+        velocity_plot.push_back(states[i](1));
+
+    }
+
+
+ // plot position
+    ct::core::plot::subplot(3, 1, 1);
+     ct::core::plot::title("Simulation Results");
+    ct::core::plot::labelPlot("Position", time_plot, position_plot);
+    ct::core::plot::legend();
+    ct::core::plot::ylabel("Position");
+     ct::core::plot::xlabel("Time");
+    // plot velocity
+    ct::core::plot::subplot(3, 1, 2);
+    ct::core::plot::labelPlot("Velocity", time_plot, velocity_plot);
+    ct::core::plot::legend();
+    ct::core::plot::ylabel("Velocity");
+     ct::core::plot::xlabel("Time");
+        // plot velocity
+    ct::core::plot::subplot(3, 1, 3);
+    ct::core::plot::labelPlot("Phase Plot",velocity_plot,position_plot);
+    ct::core::plot::legend();
+    ct::core::plot::ylabel("Velocity");
+     ct::core::plot::xlabel("Position");
+
+    ct::core::plot::show();
+
+return 0;
+
+
+}
